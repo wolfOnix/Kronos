659aaa080f319895cef15ea9da87af951b228aa7
Robert Kovats <robertrobertzicku@yahoo.com>
2023-05-07 21:31:23 +0300

    "Test notification" button from settings is now always shown. UI updates for Settings.

--- app/src/main/java/com/roko/kronos/ui/view/SettingsView.kt
+++ app/src/main/java/com/roko/kronos/ui/view/SettingsView.kt
@@ -2,11 +2,16 @@ package com.roko.kronos.ui.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.roko.kronos.R
 import com.roko.kronos.globalPrefs
@@ -19,7 +24,10 @@ import com.roko.kronos.processor.notification.NotificationProcessor.postNotifica
 // todo - ensure that the Work does not remain active when notificationsEnabled is false (and false is the desired value)
 
 @Composable fun SettingsView(navController: NavController) {
-    Column {
+    Column(
+        modifier = Modifier.padding(horizontal = 20.dp, vertical = 40.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         var notificationsEnabled by remember { mutableStateOf(globalPrefs.SETTING_NOTIFICATIONS_ENABLED) } // todo - if notifications are enabled, check and request permission (SDK 33+)
 
         Row(
@@ -41,20 +49,18 @@ import com.roko.kronos.processor.notification.NotificationProcessor.postNotifica
                 }
             )
         }
-
-        if (notificationsEnabled) {
-            TextButton(
-                stringRes = R.string.test_notification,
-                onClick = {
-                    postNotification(
-                        NotificationData(
-                            title = "Test notification",
-                            preview = "Device clock out of sync...",
-                            content = "The device clock is 3 years and 4 seconds behind the network clock. Tap the notification to solve the abominable issue!"
-                        )
+        Spacer(modifier = Modifier.height(20.dp))
+        TextButton(
+            stringRes = R.string.test_notification,
+            onClick = {
+                postNotification(
+                    NotificationData(
+                        title = "Test notification",
+                        preview = "Device clock out of sync...",
+                        content = "The device clock is 3 years and 4 seconds behind the network clock. Tap the notification to solve the abominable issue!"
                     )
-                }
-            )
-        }
+                )
+            }
+        )
     }
 }
