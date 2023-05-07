0d44e924bca83805f356b9b91dac6d4499891d6d
Robert Kovats <robertrobertzicku@yahoo.com>
2023-05-07 21:48:02 +0300

    No network text added back. Commented legacy code removed.

--- app/src/main/java/com/roko/kronos/ui/view/HomeView.kt
+++ app/src/main/java/com/roko/kronos/ui/view/HomeView.kt
@@ -45,9 +45,14 @@ import kotlin.math.absoluteValue
     ) {
         Row(modifier = Modifier.padding(20.dp)) {
             Clock(titleRes = R.string.device_clock, time = deviceTime)
-            Clock(titleRes = R.string.real_clock, time = networkTime ?: "?")
+            Clock(titleRes = R.string.real_clock, time = networkTime ?: "- - -")
         }
-        if (networkTime != null) {
+        if (clockState == ClockState.NO_INTERNET) {
+            CentredText(
+                text = stringResource(id = R.string.time_cannot_be_checked_),
+                colour = Colour.ORANGE.auto()
+            )
+        } else if (networkTime != null) {
             timeDelta.let { (deltaMillis, deltaString) ->
                 if (deltaMillis != null) {
                     if (deltaMillis.absoluteValue >= 10L && deltaString != null) { // todo - replace precision with user's defined precision
@@ -73,46 +78,6 @@ import kotlin.math.absoluteValue
                 }
             }
         }
-
-        /* // CentredText(text = stringResource(id = R.string.hello))
-        networkTimeMillis.let {
-            // updateTime()
-            Row(modifier = Modifier.padding(20.dp)) {
-                Clock(titleRes = R.string.device_clock, time = deviceTimeMillis.asTimeString(context = context))
-                if (isNetworkConnection) Clock(titleRes = R.string.real_clock, time = it?.asTimeString(context = context) ?: "--:--")
-            }
-            if (!isNetworkConnection) {
-                CentredText(
-                    text = stringResource(id = R.string.time_cannot_be_checked_),
-                    colour = Colour.ORANGE.auto()
-                )
-            } else if (it != null) {
-                val timeDifference = (it - deviceTimeMillis) / 1000L
-                (timeDifference.absoluteValue to timeDifference.asDifferenceString()).let { (absDiffAsSec, diffString) ->
-                    if (absDiffAsSec >= 10L && diffString != null) {
-                        CentredText(
-                            text = stringResource(
-                                id = R.string.your_device_clock_is_,
-                                diffString,
-                                stringResource(id = if (timeDifference >= 0L) R.string.behind else R.string.ahead)
-                            ),
-                            colour = when (absDiffAsSec) {
-                                in 10L until 30L -> Colour.YELLOW.auto()
-                                in 30L until 1200 -> Colour.ORANGE.auto()
-                                else -> Colour.RED.auto()
-                            }
-                        )
-                    } else {
-                        CentredText(
-                            text = stringResource(id = R.string.your_device_clock_is_synchronised_),
-                            colour = Colour.GREEN.auto()
-                        )
-                    }
-                }
-                Spacer(modifier = Modifier.height(20.dp))
-            }
-        } */
-
         CentredText(
             text = stringResource(id = R.string.auto_time_set_is, stringResource(id = if (isAutoTimeEnabled) R.string.enabled else R.string.disabled)),
             colour = if (isAutoTimeEnabled) likeOnBackground() else Colour.RED.auto()
@@ -121,7 +86,6 @@ import kotlin.math.absoluteValue
         CentredText(
             text = stringResource(id = R.string.to_synchronise_the_clock_, stringResource(id = if (isAutoTimeEnabled) R.string.disable_and_re_enable else R.string.enable))
         )
-        // CentredText(text = "* ${stringResource(id = R.string.the_option_could_be_named_differently)}", colour = Colour.GRAY.auto(), fontSizeInt = 13)
         Spacer(modifier = Modifier.height(20.dp))
         TextButton(
             stringRes = R.string.open_time_settings,
